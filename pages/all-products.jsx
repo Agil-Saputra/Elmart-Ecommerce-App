50301f5644559fbb5f0e9a5794febfb1781125b1
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-05-16 08:05:56 +0700

    fix deploy

--- pages/all-products.jsx
+++ pages/all-products.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
-import { contentfulClient } from '@/cms/contentful';
-import ProductCard from '@/components/card/productCard';
-
+import React from "react";
+import { contentfulClient } from "@/cms/contentful";
+import ProductCard from "@/components/card/productCard";
+import { cartState } from "@/context/Provider";
 
 export async function getStaticProps() {
-
-  const data = await contentfulClient("product")
+  const data = await contentfulClient("product");
   return {
     props: {
       products: data.items,
@@ -13,37 +12,50 @@ export async function getStaticProps() {
   };
 }
 
-const AllProducts = ({products}) => {
-  const [value, setValue] = useState("")
-  function filtered (arr) {
-    return arr.filter(item => item.fields.title.toLowerCase().includes(value))
+const AllProducts = ({ products }) => {
+  const {
+    state: { searchQuery },
+  } = cartState();
+  function filtered(arr) {
+    return arr.filter(
+      (item) =>
+        item.fields.title.toLowerCase().includes(searchQuery) ||
+        item.fields.description.toLowerCase().includes(searchQuery)
+    );
   }
 
-  const filteredProducts = filtered(products)
-
+  const filteredProducts = filtered(products);
   return (
-    <div className='margin-top-global main-margin'>
-    <form>
-      <input className='border-2 outline-sky-50' type="text" onChange={(e) => setValue(e.target.value)} />
-    </form>
-    <div className='border-2 p-2'>
-    <p>filtered products</p>
-      {filteredProducts.map(item => {
-        const {title, slug, price, categoryref, description, productImages} = item.fields
-        return (
-          <ProductCard 
-            title={title}
-            slug={slug}
-            price={price}
-            desc={description}
-            category={categoryref[0].fields.title}
-            image={productImages[0].fields.file.url}
-          />
-        )
-      })}
+    <div className="margin-top-global main-margin">
+      {filteredProducts[0]
+       ? ( <div className="gap-2 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+          {filteredProducts.map((item) => {
+            const {
+              title,
+              slug,
+              price,
+              categoryref,
+              description,
+              productImages,
+            } = item.fields;
+            return (
+              <ProductCard
+                key={slug}
+                title={title}
+                slug={slug}
+                price={price}
+                desc={description}
+                category={categoryref[0].fields.title}
+                image={productImages[0].fields.file.url}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div>sorry we couldnt find your product, try another keyword</div>
+      )}
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default AllProducts
+export default AllProducts;
